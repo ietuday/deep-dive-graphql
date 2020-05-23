bd07ee96a1825442b287f0393a79deba01d09b8e
Udayaditya Singh <udayaditya.singh@gmail.com>
2020-05-23 09:03:45 +0530

    "changes"

--- graphql-bootcamp/graphql-basics/src/index.js
+++ graphql-bootcamp/graphql-basics/src/index.js
@@ -1,9 +1,75 @@
-import { message,name } from './my-module';
-import myAddFunction, { subtract } from './math'
+import { GraphQLServer } from "graphql-yoga";
+/*
+  SCALAR TYPE
+    String
+    Boolean
+    Int
+    Float
+    ID
+  
+  
 
-console.log(message);
-console.log(name);
+*/
 
 
-console.log(myAddFunction(1, -2))
-console.log(subtract(10, 2))
+//Type Definations 
+const typeDefs = `
+    type Query{
+        greeting(name: String): String!
+        me:User!
+        post: Post! 
+    }
+
+    type User{
+      id: ID!
+      name: String!
+      email: String!
+      age: Int
+    }
+
+    type Post{
+      id: ID!
+      title: String!
+      body: String!
+      published: String!
+    }
+`
+
+//Resolvers
+const resolvers = {
+    Query: {
+      greeting(parents, args, ctx, info){
+        console.log(parents);
+        console.log(args);
+        console.log(ctx);
+        console.log(info);
+        if(args.name) return `Hello ${args.name}`
+        else return 'Hello'
+      },
+      me(){
+        return {
+          id: '12308',
+          name: 'Uday',
+          email: 'udayaditya@gmail.com',
+          age: 28
+        }
+      },
+      post(){
+        return{
+          id: '092',
+          title: 'GRAPHQL AMAZING',
+          body: '',
+          published: false
+        }
+      }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => {
+    console.log("Graphql Server is UP");
+})
---
+++ graphql-bootcamp/graphql-basics/src/index1.js
@@ -0,0 +1,9 @@
+import { message,name } from './my-module';
+import myAddFunction, { subtract } from './math'
+
+console.log(message);
+console.log(name);
+
+
+console.log(myAddFunction(1, -2))
+console.log(subtract(10, 2))
