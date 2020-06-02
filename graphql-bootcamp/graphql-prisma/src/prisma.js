5145c8f97bf9c8bf9d23e7edf976a6b5c5db330c
Udayaditya Singh <udayaditya.singh@gmail.com>
2020-06-02 21:56:57 +0530

    "password encrypt"

--- graphql-bootcamp/graphql-prisma/src/prisma.js
+++ graphql-bootcamp/graphql-prisma/src/prisma.js
@@ -2,7 +2,8 @@ import { Prisma } from 'prisma-binding'
 
 const prisma = new Prisma({
     typeDefs: 'generated/prisma.graphql',
-    endpoint: 'localhost:4466'
+    endpoint: 'localhost:4466',
+    secret: 'thisismysupersecrettext'
 })
 
 
